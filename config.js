4f8d63daca55035be56aa2041b0c796146046a24
jonhicksdesign <70168774+jonhicksdesign@users.noreply.github.com>
2021-01-26 16:39:39 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '500628810899095',
+        clientSecret: '25138fc816bbca35bc8b209758c7dc58'
+    }
+}
